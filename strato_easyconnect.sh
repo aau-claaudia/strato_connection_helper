244761622f601ca0d6df12cbd4117f17c0224ead
Sighvatur Sveinn Davidsson <sdavidsson90@gmail.com>
2023-12-20 18:28:00 +0100

    32210

--- strato_easyconnect.sh
+++ strato_easyconnect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --
-AAU_UPN="x00xx@domain.aau.dk" # Replace with your AAU UPN
+AAU_UPN="x00xx@domain.aau.dk"
 IDENTITY_FILE="$HOME/.ssh/my_key" # Replace with path of your SSH-key
 
 # --
@@ -13,7 +13,7 @@ CONTROLMASTER="-o ControlMaster=auto  -o ControlPath='~/.ssh/controlmasters/%r@%
 
 # -- 
 if ! ping -c 1 ai-fe02.srv.aau.dk &> /dev/null; then
-  COMMAND="ssh -J $AAU_UPN@sshgw.aau.dk $CONTROLMASTER -i $IDENTIFIYFILE ubuntu@10.92."
+  COMMAND="ssh -J $AAU_UPN@sshgw.aau.dk $CONTROLMASTER -i $IDENTITY_FILE ubuntu@10.92."
   echo -n $COMMAND; read LAST_BIT
   eval $COMMAND$LAST_BIT
 else  
