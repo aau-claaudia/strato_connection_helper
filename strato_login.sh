234ba10c6dc147657e33059187cff72e615fecdd
Sighvatur Sveinn Davidsson <sdavidsson90@gmail.com>
2024-11-18 11:42:35 +0100

    update

--- strato_login.sh
+++ strato_login.sh
@@ -1,79 +1,64 @@
+
 #!/bin/bash
 
-# ==========================================
-# Variables
+# ==================================
+# -- VARIABLES
+AAU_UPN='cc11gz@its.aau.dk'
+ssh_key="$HOME/Filen_io/ssd_env/ssh/keys/generic/ssd_24q3"
+host_user='ubuntu'
+first_bit='10.92.'
+login_history="$(dirname ${BASH_SOURCE[0]})/.strato_login_history"
+jumphost_arg=""
+ctrlm_time=28800
+ctrlm_path="$HOME/.ssh/controlmasters"; mkdir -p $ctrlm_path
+ctrlm_arg="-o ControlMaster=auto -o ControlPath=$ctrlm_path/%r@%h -o ControlPersist=$ctrlm_time"
 
-# -- Mandatory:
-# Change this to your actual AAU UPN
-AAU_UPN="aa00xx@its.aau.dk"
-
-# Change this to the location of your ssh-key
-IDENTITY_FILE="$HOME/.ssh/id_rsa"
-
-
-# -- Optional
-# How long should the connection be kept alive for?
-CONTROLMASTER_TIME=3600 
-
-# If the remote host has a different username, this can be set
-HOST_USERNAME="ubuntu" 
-
-# If remote ip is not in the 10.92 area, this can be set
-HOST_IP="10.92." 
-
-# No need to change anything below this line
-# ==========================================
-
-# cd to the location of script
-cd $(dirname $0)
-
-# -------------------------------------------
-# Identify network
+# Determine if we are on aau network
 if ! ping -c 1 ai-fe02.srv.aau.dk &> /dev/null; then
-  AAU_NETWORK="-J $AAU_UPN@sshgw.aau.dk"
-else
-  AAU_NETWORK=""
+  jumphost_arg="-J $AAU_UPN@sshgw.aau.dk"
 fi
 
-# ------------------------------------------
-# Controlmaster chunk
-CONTROLPATH="$HOME/.ssh/controlmasters" ; mkdir -p $CONTROLPATH
-CONTROLMASTER="-o ControlMaster=auto -o ControlPath=$CONTROLPATH/%r@%h -o ControlPersist=$CONTROLMASTER_TIME"
+# ==================================
+# -- READ 
+# Gather command
+ssh_cmd="ssh $jumphost_arg -i $ssh_key $@ $host_user@$first_bit"
 
-# Locate $SOCKET_FILE (if exists)
-SOCKET_FILE=$(find $CONTROLPATH -type s -name "*$HOST_USERNAME*$HOST_IP$LAST_BIT")
+# Print ssh-command
+echo -n "$ssh_cmd"
 
-# Determine age of $SOCKET_FILE
-if [ ! -z $SOCKET_FILE ]; then
-  if [ "$(uname)" = "Darwin" ]; then
-    SOCKET_FILE_AGE=$(stat -f "%m" $SOCKET_FILE)
-  elif [ "$(uname)" = "Linux" ]; then
-    SOCKET_FILE_AGE=$(stat -c %Y $SOCKET_FILE)
-  fi
-  SOCKET_TIME_LEFT=$(echo "$(date +%s) - $SOCKET_FILE_AGE" | bc)
-  echo "Control socket: $SOCKET_TIME_LEFT/$CONTROLMASTER_TIME sec"
-  echo ""
-fi
+# Read last bit
+last_bit=$(bash -c "read last_bit; echo \$last_bit")
 
-# -------------------------------------------
-# Print ssh-command and read last bit of IP
-COMMAND="ssh $AAU_NETWORK -i $IDENTITY_FILE $HOST_USERNAME@$HOST_IP"
-
-echo -e "$COMMAND\c"
-LAST_BIT=$(bash -c "read last_bit; echo \$last_bit")
-
-if [[ -z "$LAST_BIT" ]]; then
-  if [[ ! -f ".strato_login_history" ]]; then
-    echo -e "\nAborting! Remote host IP wasn't finished, and no login history file was found."
+# Find the two last bits of the IP 
+if [[ -z "$last_bit" ]]; then
+  if [[ -f "$login_history" ]]; then
+    # Get last bit from file
+    last_bit=$(awk -F "." 'END { print $3 "." $4 }' < $login_history)
+    ssh_cmd="${ssh_cmd}${last_bit}"
+    echo -en "\033[1A\033[2K"   # Removes previous line (which is empty because user hit enter)
+    echo -e "$ssh_cmd"          # Prints the complete command to the line
+  else
+    echo -e "\nRemote host IP was not finished, and no login history file was found."
     exit 1
   fi
-  echo -en "\033[1A\033[2K"
-  LAST_BIT=$(awk -F "." 'END { print $3 "." $4 }' < .strato_login_history)
-  echo -e "$COMMAND$LAST_BIT"
 fi
 
-# ------------------------------------------
-ssh $AAU_NETWORK $CONTROLMASTER -i $IDENTITY_FILE $HOST_USERNAME@$HOST_IP$LAST_BIT
+# ==================================
+# -- CONNECT
+# Check if socket file exists
+ctrlm_socket_file=$(find "$ctrlm_path" -type s -name "*$user_name@$first_bit$last_bit*")
 
-# ------------------------------------------
-echo "$(date "+%Y-%m-%d %H:%M")  $HOST_USERNAME@$HOST_IP$LAST_BIT" >> .strato_login_history
+# Check if the socket file works - if not, remove it.
+if [[ -n $ctrlm_socket_file ]]; then
+  timeout 1 ssh $host hostname &> /dev/null
+  if [[ $? > 0 ]]; then
+    rm $ctrlm_socket_file
+  fi
+fi
+
+eval $ssh_cmd
+
+# ==================================
+# -- LOG
+# Log successful connection 
+echo "$(date "+%Y-%m-%d_%H:%M") $host_user@$first_bit$last_bit" > $login_history
